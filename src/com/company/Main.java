b63531350c67f226a2b66129959b1e8b3098f457
Raimonds Bortascenoks <r.bortascenoks@gmail.com>
2019-12-03 20:30:55 +0200

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int nameCount = 96;
+        int nameCountTwo = 0;
+        int nameCountOne = 40;
+        nameCount = nameCount + nameCountOne;
+        System.out.println(nameCount+nameCountOne);
+        System.out.println(nameCountTwo);
+    }
+}
